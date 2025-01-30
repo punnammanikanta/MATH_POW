251f3fc8ddcdf455f611f3766a5b2f88d93b5aa4
punnammanikanta <PUNNAMMANIKANTA52@GMAIL.COM>
2025-01-30 12:12:22 +0530

    Add files via upload

---
+++ Math_Pow.java
@@ -0,0 +1,8 @@
+public class Math_Pow {
+         public static void main(String[] args) {
+            int p = (int) Math.pow(83, 2);
+            System.out.printf("83 to the power of 2 is %d", p);
+        }
+    }
+    
+
